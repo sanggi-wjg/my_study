006278fc3b428578f5a9b63b57024847fb5db133
sanggi-wjg <girr311@naver.com>
2021-08-24 17:39:04 +0900

    2021 08 24

---
+++ 5.형식_맞추기/test/5-1-test.go
@@ -0,0 +1,38 @@
+package test
+
+import "fmt"
+
+type Something struct {
+	some  string
+	thing string
+}
+
+func NewSomething(some string, thing string) *Something {
+	s := Something{
+		some:  "Some",
+		thing: "Thing",
+	}
+	return &s
+}
+
+func (s *Something) Some() string {
+	return s.some
+}
+
+func (s *Something) Thing() string {
+	return s.thing
+}
+
+func (s *Something) DoThing() {
+	fmt.Println("Do " + s.thing)
+}
+
+func (s *Something) DoAnyThing() {
+	ThingThingThing(s)
+}
+
+func ThingThingThing(s *Something) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s.Thing())
+	}
+}
